668dd99fe4446bebc6b3813db1fd1f4ba2862b0d
elanbmiller <elanbmiller@gmail.com>
2016-11-20 20:51:28 -0600

    Keyboard UI

---
+++ keyboardUI/SimpleSynth/FeedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ThirdViewController.swift
+//  SimpleSynth
+//
+//  Created by Labuser on 11/15/16.
+//  Copyright © 2016 wustl. All rights reserved.
+//
+
+import UIKit
+
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //view.backgroundColor = UIColor.blueColor()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
